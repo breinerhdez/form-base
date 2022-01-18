51416acc6c4f2eaabc74c648a46ac9c4863b529d
Breiner E. Hernández G <ing.breiner.hernandez@gmail.com>
2022-01-17 22:20:23 -0500

    Fields config

--- controllers/FieldsConfigController.js
+++ controllers/FieldsConfigController.js
@@ -12,20 +12,84 @@ const breadItems = [
 ];
 var viewData = { getRoute, paths, title: "Fields", breadItems };
 
-const index = (req, res) => {
+const index = async (req, res) => {
+  // get params
   let { id } = req.params;
   let fieldsPath = fieldsMainPath + id;
 
-  let data = { ...viewData, fieldsPath };
+  // validate object existence
+  let objDb = await CoreCollectionsModel.findById(id);
+  if (!objDb) {
+    req.flash("info", lang.CRUD_NOT_EXIST);
+    return res.redirect(getRoute(paths, "index"));
+  }
+
+  let data = { ...viewData, fieldsPath, collection: objDb };
   res.render(`fieldsConfig/index`, data);
 };
 
-const update = (req, res) => {
-  let { id } = req.params;
-  let fieldsPath = fieldsMainPath + id;
+const update = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let fieldsPath = fieldsMainPath + id;
 
-  req.flash("info", "Fields updated");
-  res.redirect(fieldsPath);
+    // validate object existence
+    let objDb = await CoreCollectionsModel.findById(id);
+    if (!objDb) {
+      req.flash("info", lang.CRUD_NOT_EXIST);
+      return res.redirect(getRoute(paths, "index"));
+    }
+
+    let reqFields = req.body.field;
+    let objFields = [];
+    let labels = [];
+
+    for (let i = 0; i < reqFields.name.length; i++) {
+      let field = {
+        name: reqFields.name[i],
+        type: reqFields.type[i],
+        label: reqFields.label[i],
+        cols: reqFields.cols[i],
+        default_value: reqFields.default_value[i],
+        projection: reqFields.projection.includes(reqFields.name[i]),
+      };
+
+      if (reqFields.projection.includes(field.name)) {
+        labels.push(field.label);
+      }
+
+      objFields.push(field);
+    }
+
+    objDb.collectionConfig.projection = reqFields.projection.join(" ");
+    objDb.collectionConfig.labels = labels;
+    objDb.form.fields = objFields;
+
+    await objDb.save();
+
+    req.flash("info", "Fields updated");
+
+    console.log(req.body);
+    console.log(objDb);
+    console.log(objFields);
+    // res.send(JSON.stringify(req.body));
+
+    res.redirect(fieldsPath);
+  } catch (error) {
+    console.log(error.message);
+    if (error.name === "ValidationError") {
+      req.flash(
+        "danger",
+        Object.values(error.errors)
+          .map((val) => val.message)
+          .join("<br>")
+      );
+      res.redirect(fieldsPath);
+    } else {
+      req.flash("warning", lang.ERROR_500);
+      res.redirect(getRoute(paths, "index"));
+    }
+  }
 };
 
 module.exports = { index, update };
---
+++ public/javascripts/fieldsConfig.js
@@ -0,0 +1,56 @@
+// form for new field
+const newFieldForm = `<tr>
+<td>
+    <input type="text" name="field[name][]" class="fieldNameRow" placeholder="name_field" required>
+</td>
+<td>
+    <input type="text" name="field[label][]" placeholder="Label field" required>
+</td>
+<td>
+    <select name="field[type][]" required>
+        <option value="">Select an option</option>
+        <option value="Email">Email</option>
+        <option value="Number">Number</option>
+        <option value="Password">Password</option>
+        <option value="Text">Text</option>
+    </select>
+</td>
+<td>
+    <input type="text" name="field[default_value][]" placeholder="Default field value">
+</td>
+<td>
+    <input type="text" name="field[cols][]" placeholder="col-md-12" required>
+</td>
+<td>
+    <input type="checkbox" name="field[projection][]" class="projectionCheckboxRow">
+</td>
+<td>
+    <span class="btn btn-danger fa fa-trash btn-removeField" title="Delete"></span>
+</td>
+</tr>`;
+
+$(document).ready(function () {
+  // sortable for fields rows
+  $("#rowFieldsSortable").sortable();
+});
+
+// event handler for add field
+$("#btnAddField").on("click", function () {
+  $("#fieldsTable tbody").append(newFieldForm);
+});
+
+// event handler for remove field
+$(document).on("click", ".btn-removeField", function () {
+  if (confirm("Are you sure you want to delete this object?")) {
+    let row = $(this).parents("tr");
+    row.hide(800, function () {
+      $(this).remove();
+    });
+  }
+});
+
+// event handler for change value in name input and replace value for projection input
+$(document).on("change", ".fieldNameRow", function () {
+  let prjCheckbox = $(this).parents("tr").find(".projectionCheckboxRow");
+  prjCheckbox.attr("value", $(this).val());
+});
