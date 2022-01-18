14c2fb6125a51ee76c80e55617eeb6f67a043512
Breiner E. Hernández G <ing.breiner.hernandez@gmail.com>
2022-01-18 00:22:42 -0500

    Documentation

--- controllers/AutoCrudController.js
+++ controllers/AutoCrudController.js
@@ -1,23 +1,30 @@
+const lang = require("../utils/lang");
 const Form = require("../lib/form/Form");
 const { getObjectsAndModel } = require("../utils/dynamicResources");
 
 const index = async (req, res) => {
-  let { path_name } = req.params;
-
-  let result = await getObjectsAndModel(path_name, req);
-  if (!result) {
+  try {
+    // get path_name
+    let { path_name } = req.params;
+    // get main information for colletion by path_name
+    let result = await getObjectsAndModel(path_name, req);
+    if (!result) {
+      res.redirect("/admin");
+    }
+    // destructure result query into variables
+    let { collection } = result;
+    // generate form object
+    let objForm = new Form("#", collection.form.fields); // TODO: set action and remove, this is for create or edit
+    // set view
+    res.render("autoCrud/index", {
+      title: collection.title,
+      collection,
+      objForm, // TODO: delete
+    });
+  } catch (error) {
+    req.flash("warning", lang.ERROR_500);
     res.redirect("/admin");
   }
-
-  let { collection } = result;
-
-  let objForm = new Form("#", collection.form.fields);
-
-  res.render("autoCrud/index", {
-    title: collection.title,
-    collection,
-    objForm,
-  });
 };
 
 module.exports = { index };
