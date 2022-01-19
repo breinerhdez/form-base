077ff3d80b2bd193c32a23b4a1251e68cfe3e391
Breiner E. Hernández G <ing.breiner.hernandez@gmail.com>
2022-01-18 22:20:50 -0500

    AutoCRUD dummy responses

--- controllers/AutoCrudController.js
+++ controllers/AutoCrudController.js
@@ -1,6 +1,11 @@
 const lang = require("../utils/lang");
 const Form = require("../lib/form/Form");
 const { getObjectsAndModel } = require("../utils/dynamicResources");
+const { getAutocrudRoute } = require("../utils/helpers");
+
+const basePath = "/admin/crud";
+
+var viewData = { getAutocrudRoute, basePath };
 
 const index = async (req, res) => {
   try {
@@ -13,18 +18,132 @@ const index = async (req, res) => {
     }
     // destructure result query into variables
     let { collection } = result;
-    // generate form object
-    let objForm = new Form("#", collection.form.fields); // TODO: set action and remove, this is for create or edit
+
     // set view
-    res.render("autoCrud/index", {
-      title: collection.title,
-      collection,
-      objForm, // TODO: delete
-    });
+    let data = { ...viewData, title: collection.title, collection };
+    res.render("autoCrud/index", data);
   } catch (error) {
     req.flash("warning", lang.ERROR_500);
     res.redirect("/admin");
   }
 };
 
-module.exports = { index };
+const create = async (req, res) => {
+  try {
+    // get path_name
+    let { path_name } = req.params;
+    // get main information for colletion by path_name
+    let result = await getObjectsAndModel(path_name, req);
+    if (!result) {
+      res.redirect("/admin");
+    }
+    // destructure result query into variables
+    let { collection } = result;
+    // generate form object
+    let objForm = new Form(
+      getAutocrudRoute(basePath, "store", collection.path_name),
+      collection.form.fields
+    );
+    // set view
+    let data = { ...viewData, title: collection.title, collection, objForm };
+    res.render("autoCrud/create", data);
+  } catch (error) {
+    req.flash("warning", lang.ERROR_500);
+    res.redirect("/admin");
+  }
+};
+
+const store = async (req, res) => {
+  try {
+    // get path_name
+    let { path_name } = req.params;
+    // get main information for colletion by path_name
+    let result = await getObjectsAndModel(path_name, req);
+    if (!result) {
+      res.redirect("/admin");
+    }
+    // destructure result query into variables
+    let { collection } = result;
+
+    console.log(req.body);
+    req.flash("info", "Data was stored");
+    res.redirect(getAutocrudRoute(basePath, "index", collection.path_name));
+  } catch (error) {
+    req.flash("warning", lang.ERROR_500);
+    res.redirect("/admin");
+  }
+};
+
+const edit = async (req, res) => {
+  try {
+    // get path_name
+    let { path_name } = req.params;
+    // get main information for colletion by path_name
+    let result = await getObjectsAndModel(path_name, req);
+    if (!result) {
+      res.redirect("/admin");
+    }
+    // destructure result query into variables
+    let { collection } = result;
+    // generate form object
+    let objForm = new Form(
+      getAutocrudRoute(basePath, "update", collection.path_name, "asdf123"),
+      collection.form.fields
+    );
+    // set view
+    let data = {
+      ...viewData,
+      title: collection.title + "EDIT",
+      collection,
+      objForm,
+    };
+    res.render("autoCrud/create", data);
+  } catch (error) {
+    req.flash("warning", lang.ERROR_500);
+    res.redirect("/admin");
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    // get path_name
+    let { path_name } = req.params;
+    // get main information for colletion by path_name
+    let result = await getObjectsAndModel(path_name, req);
+    if (!result) {
+      res.redirect("/admin");
+    }
+    // destructure result query into variables
+    let { collection } = result;
+
+    console.log(req.body);
+    req.flash("info", "Data was updated");
+    res.redirect(getAutocrudRoute(basePath, "index", collection.path_name));
+  } catch (error) {
+    req.flash("warning", lang.ERROR_500);
+    res.redirect("/admin");
+  }
+};
+
+const destroy = async (req, res) => {
+  try {
+    // get path_name
+    let { path_name } = req.params;
+    // get main information for colletion by path_name
+    let result = await getObjectsAndModel(path_name, req);
+    if (!result) {
+      res.redirect("/admin");
+    }
+    // destructure result query into variables
+    let { collection } = result;
+
+    console.log(req.body);
+    req.flash("info", "Data was deleted");
+    res.redirect(getAutocrudRoute(basePath, "index", collection.path_name));
+  } catch (error) {
+    req.flash("warning", lang.ERROR_500);
+    res.redirect("/admin");
+  }
+};
+
+module.exports = { index, create, store, edit, update, destroy };
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,23 @@
 // Patterns routes for web application
 const crudAppPatterns = {
-  index: "/",
-  create: "/create",
-  store: "/",
-  edit: "/:id/edit",
-  update: "/:id",
-  destroy: "/:id",
-  show: "/:id",
+  index: "/", // GET
+  create: "/create", // GET
+  store: "/", // POST
+  edit: "/:id/edit", // GET
+  update: "/:id", // POST
+  destroy: "/:id", // GET
+  show: "/:id", // GET
+};
+
+// Patterns routes for autocrud web application
+const autoCrudAppPatterns = {
+  index: "/:path_name", // GET
+  create: "/:path_name/create", // GET
+  store: "/:path_name", // POST
+  edit: "/:path_name/:id/edit", // GET
+  update: "/:path_name/:id", // POST
+  destroy: "/:path_name/:id", // GET
+  show: "/:path_name/:id", // GET
 };
 
 // Application routes for web application
@@ -22,12 +33,24 @@ const crudAppRoutes = (main) => {
 };
 
 // Get route by name option
+// TODO: update like getAutocrudRoute
 const getRoute = (paths, option, id = null) => {
   return id ? paths[option].replace("/:id", `/${id}`) : paths[option];
+};
+
+// Get route by name option and dynamic path_name for AutoCRUD
+const getAutocrudRoute = (basePath, option = "index", path_name, id = null) => {
+  let route =
+    basePath +
+    autoCrudAppPatterns[option].replace("/:path_name", `/${path_name}`);
+  route = id ? route.replace("/:id", `/${id}`) : route;
+  return route;
 };
 
 module.exports = {
   crudAppPatterns,
   crudAppRoutes,
+  autoCrudAppPatterns,
+  getAutocrudRoute,
   getRoute,
 };
